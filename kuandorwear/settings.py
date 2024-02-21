f4e93136638f6010b82f246002fa2cd39c3c11a4
emmanuel197 <66939968+emmanuel197@users.noreply.github.com>
2024-02-21 19:40:04 +0000

    Update settings.py

--- kuandorwear/settings.py
+++ kuandorwear/settings.py
@@ -180,6 +180,9 @@ DJOSER = {
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'store/static')
 
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
