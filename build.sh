ce8f7480956f956aec2d4c4a439c583c145d7695
emmanuel197 <66939968+emmanuel197@users.noreply.github.com>
2024-02-22 13:33:01 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@ echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
-# echo "Collect Static..."
+# echo "Create superuser"
 # python3.9 manage.py collectstatic --noinput --clear
+python3.9 manage.superuser()
