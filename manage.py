7e1feecc6cd7b9ceb8c899e501d05bb64f40d1f3
emmanuel197 <66939968+emmanuel197@users.noreply.github.com>
2024-02-22 13:20:27 +0000

    Update manage.py

--- manage.py
+++ manage.py
@@ -16,7 +16,22 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
+    
+def superuser(event, context):
+    email = os.getenv('SUPERUSER_EMAIL')
+    password = os.getenv('SUPERUSER_PASSWORD')
+    
+    if email and password:
+        call_command('createsuperuser', interactive=False, email=email, password=password)
+        return {
+            'statusCode': 200,
+            'body': 'Superuser created successfully'
+        }
+    else:
+        return {
+            'statusCode': 500,
+            'body': 'Error: SUPERUSER_EMAIL or SUPERUSER_PASSWORD environment variables are not set.'
+        }
 
 if __name__ == '__main__':
     main()
