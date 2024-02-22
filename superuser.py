3b91f3933e213f95fd7abae07dc27e75063c8a9a
emmanuel197 <66939968+emmanuel197@users.noreply.github.com>
2024-02-22 14:53:33 +0000

    Update superuser.py

--- superuser.py
+++ superuser.py
@@ -1,1 +1,19 @@
+from django.core.management import call_command
 
+def superuser(event, context):
+    email = os.getenv('SUPERUSER_EMAIL')
+    password = os.getenv('SUPERUSER_PASSWORD')
+    
+    if email and password:
+        call_command('createsuperuser', interactive=False, email=email, password=password)
+        return {
+            'statusCode': 200,
+            'body': 'Superuser created successfully'
+        }
+    else:
+        return {
+            'statusCode': 500,
+            'body': 'Error: SUPERUSER_EMAIL or SUPERUSER_PASSWORD environment variables are not set.'
+        }
+
+def ""
