939806e9aa5631b7884b468547ee2ef4c42a38c7
emmanuel197 <66939968+emmanuel197@users.noreply.github.com>
2024-02-22 13:36:08 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Build the project
-# echo "Building the project..."
-# python3.9 -m pip install -r requirements.txt
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
 
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
