096450d708b2cfc6587a1ae3589ba14ea9b2b678
emmanuel197 <66939968+emmanuel197@users.noreply.github.com>
2024-02-28 22:09:49 +0000

    Update Layout.js

--- store/src/hocs/Layout.js
+++ store/src/hocs/Layout.js
@@ -2,31 +2,36 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { checkAuthenticated, load_user } from '../actions/auth';
 import Navbar from '../components/Navbar';
+import { fetchCartData, fetchCookieCart } from '../actions/cartActions';
+import { fetchProductsData, fetchProductDetail } from '../actions/productActions';
+
 
 
 class Layout extends Component {
     componentDidMount() {
     //     // Call action creators directly without destructure
-        this.props.checkAuthenticated();
         this.props.load_user();
-    }
+        this.props.checkAuthenticated();
 
+        const productsData = JSON.parse(localStorage.getItem('productsData'))
+        if (productsData == undefined) { 
+            this.props.fetchProductsData()
+        }       
+    }
     render() {
         const { children } = this.props;
 
         return (
             <div>
-                <Navbar 
-                logged_in={this.props.logged_in}
-                cart_total_updated={this.props.cart_total_updated}
-                updatedToggler={this.props.updatedToggler}
-                cartUpdated={this.props.cartUpdated}
-                cartUpdatedToggler={this.props.cartUpdatedToggler}/>
-              
+                <Navbar />      
                 {children}
             </div>
         );
     }
 }
 
-export default connect(null, { checkAuthenticated, load_user })(Layout);
+const mapStateToProps = (state) => ({
+    isAuthenticated: state.auth.isAuthenticated
+  });
+
+export default connect(mapStateToProps, { checkAuthenticated, load_user, fetchCartData, fetchCookieCart, fetchProductsData, fetchProductDetail })(Layout);
