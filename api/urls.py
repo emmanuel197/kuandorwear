2c9ece59d4b102b159fff956589a4425ffbcf052
emmanuel197 <66939968+emmanuel197@users.noreply.github.com>
2024-02-28 00:15:53 +0000

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('products', ProductListView.as_view()),
+    path('products', ProductAPIView.as_view()),
     path('create-order/', CreateOrUpdateOrderView.as_view()),
-    path('check-auth/', AuthCheck.as_view()),
     path('cart-data/', CartDataView.as_view()),
     path('update-cart/', updateCartView.as_view()),
     path('process-order/', ProcessOrderView.as_view()),
