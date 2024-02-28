7c167910288730b9918c0c975cc1162f5185f4f9
emmanuel197 <66939968+emmanuel197@users.noreply.github.com>
2024-02-28 00:05:11 +0000

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('products', ProductListView.as_view()),
-    path('product/<int:id>', ProductDetailView.as_view()),
     path('create-order/', CreateOrUpdateOrderView.as_view()),
     path('check-auth/', AuthCheck.as_view()),
     path('cart-data/', CartDataView.as_view()),
